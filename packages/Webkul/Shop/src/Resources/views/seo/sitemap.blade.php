fca3c584ab07dd315644c9340ca383e368dec00b
Riccardo Sartori <rikki.sarto@gmail.com>
2026-09-19 18:53:27 +0200

    fix: Sitemap phhp

--- packages/Webkul/Shop/src/Resources/views/seo/sitemap.blade.php
+++ packages/Webkul/Shop/src/Resources/views/seo/sitemap.blade.php
@@ -1,4 +1,7 @@
-{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
+{{-- Split the `<?` so the PHP lexer (which the Blade compiler runs over this
+     file) can never mistake the declaration for a short open tag, whatever
+     the runtime's short_open_tag setting is. --}}
+{!! '<'.'?xml version="1.0" encoding="UTF-8"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($urls as $url)
         <url>
